3a065db70e33fb344d6e88004794171f133bd017
zhangyue <zhangyuea@hualala.com>
2023-03-27 15:47:30 +0800

    ajax请求的基本操作

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('HELLO EXPRESS');
+});
+
+app.get('/server', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send('HELLO AJAX');
+});
+
+app.listen('8000', () => {
+    console.log('服务已经启动，8000 端口监听中……');
+});
