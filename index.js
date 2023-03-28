0a2286a2e6f4974a8e3bb331aaa492ac525dae27
zhangyue <zhangyuea@hualala.com>
2023-03-28 18:18:28 +0800

    使用fetch函数发送ajax请求

--- index.js
+++ index.js
@@ -53,6 +53,16 @@ app.all('/axios-server', function(req, res) {
     // }, 5000);
 });
 
+app.all('/fetch-server', function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    let obj = {a: 'ra', b: 'rb'};
+    res.send('hello fetch===');
+    // setTimeout(() => {
+    //     res.send(JSON.stringify(obj));
+    // }, 5000);
+});
+
 app.listen('8000', () => {
     console.log('服务已经启动，8000 端口监听中……');
 });
